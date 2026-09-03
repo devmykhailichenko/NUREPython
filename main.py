5d756731ae1e1a4e24b5e4dcd7fac3ef4dfeb1b2
devmykhailichenko <ihor.mykhailichenko@gmail.com>
2026-09-03 14:49:00 +0300

    First lesson numbers

---
+++ main.py
@@ -0,0 +1,29 @@
+#Syntax
+x = int(10)
+_y = float(67.78)
+x9y = 89j
+is_admin = False
+user_name_surname = 'Ihor'
+number1, number2, number3 = 1, 2, 3
+
+print(x, _y, x9y, user_name_surname, number1, number2, number3)
+
+# numeric operations
+print(
+     6 / 4,
+    7 * 5,
+    7 ** 2,
+    12 % 10
+)
+
+number4 = 67
+number4 -= 2
+
+print(number4)
+
+print(--number4) #-(-65)
+
+number4 += 1
+print(number4)
+
+
